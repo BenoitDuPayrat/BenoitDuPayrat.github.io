9b7fad23f1e6bab8267520ebe875d264658e08c9
BenoitDuPayrat <benoit@du-payrat.com>
2022-11-13 21:42:53 +0000

    Routing

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="homePage">
+                <header className="homeHeader">
+                    <p>
+                        Bienvenue sur la Home Page !
+                    </p>
+                </header>
+            </div>
+
+        );
+    }
+
+}
+
+export default Home;
---
+++ src/Map.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const divStyle = {
+    color: 'white'
+};
+
+class Map extends React.Component {
+
+    render() {
+        return (
+            <div style={divStyle}>
+                <h2>Map Page</h2>
+                <main>
+                    <p>This section contains information about...</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+
+
+export default Map;
