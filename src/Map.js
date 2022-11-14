03c78d3950d0f9d71003f73d485cea79d27ddd80
BenoitDuPayrat <benoit@du-payrat.com>
2022-11-14 13:46:38 +0100

    Adding D3.js

--- src/Map.js
+++ src/Map.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import * as d3 from 'd3';
 
 const divStyle = {
     color: 'white'
 };
 
 class Map extends React.Component {
+    componentDidMount() {
+        let root = d3.select("#mapCanvas");
+
+        let svg = root.append("svg")
+            .attr("width", "100%")
+            .attr("height", "100%");
+
+        svg.append("circle")
+            .attr('cx', 50)
+            .attr('cy', 50)
+            .attr('r', '10px')
+            .style('fill', 'red');
+    }
 
     render() {
         return (
-            <div style={divStyle}>
+            <div style={divStyle} >
                 <h2>Carte de Todani</h2>
                 <main>
-                    <p>Il y aura la carte ici.</p>
+                    <div id="mapCanvas"></div>
                 </main>
             </div>
         )
